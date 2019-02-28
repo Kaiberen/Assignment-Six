70caac27203cd2b26a8cca3d811b129d285bd7cb
Kaiberenn <Kaiberen@hawaii.edu>
2019-02-28 08:39:20 -1000

    Initial Commit

---
+++ Assignment#SixTests/Assignment_SixTests.swift
@@ -0,0 +1,34 @@
+//
+//  Assignment_SixTests.swift
+//  Assignment#SixTests
+//
+//  Created by Kaiberenn Haleamau on 2/28/19.
+//  Copyright © 2019 Kaiberenn Haleamau. All rights reserved.
+//
+
+import XCTest
+@testable import Assignment_Six
+
+class Assignment_SixTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
